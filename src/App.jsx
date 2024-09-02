e414a960b957678f82b81aea8a93f0af16ff651b
neto <161722036+44neto@users.noreply.github.com>
2024-09-02 17:53:34 +0000

    clique

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,12 @@
 import './App.css'
 
-let alunos = [
-  {nome: 'Savalo Horse', cursos: ["DS", "ADM"]},
-  {nome: 'Miranata Filoxina', cursos:["ADM", "MA"]}
-]
-
-function App() {
+function dizerOla(){
+  alert('Olá, corinthiano!');
+}
+function App(){
   return (
     <>
-    {
-      alunos.map((element) =>(
-        <div key={ element.nome}> 
-        { element.nome }
-        {
-        element.cursos.map((curso) => (
-          <h1 key={curso}>{curso}</h1>
-        ))
-      }
-        </div>
-
-      ))
-    }
+    <button onCLick={dizerOla}>Clique Aqui</button>
     </>
   )
 }
-
-export default App
